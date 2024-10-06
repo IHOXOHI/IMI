9e400c3bd119639ac420b342430ec5b6cfa787f9
IHOXOHI <63340434+IHOXOHI@users.noreply.github.com>
2024-10-06 22:18:17 +0200

    Update IMI.py

--- IMI.py
+++ IMI.py
@@ -8,15 +8,6 @@ yea, mon, day, hou, minu, sec = tz[0],tz[1],tz[2],tz[4],tz[5],tz[6]
 
 def timing_display():
     global yea, mon, day, hou, minu, sec, t1
-    tr = rtc.datetime()
-    try:
-        hou == tr[4]
-    except:
-        hou = tr[4]
-    try:
-        minu == tr[5]
-    except:
-        minu = tr[5]
 
     now = ticks_ms()
     delai = ((now - t1) // 1000)
@@ -34,7 +25,7 @@ def timing_display():
             if hou > 60:
                 hou = hou - 60
                 day +=1
-        sec = si - 60 
+        sec = si - (60 * (si // 60)) 
 
     if (si >= 3600) and (si < 216000):
         hou = hou + (si // 3600)
@@ -48,7 +39,7 @@ def timing_display():
             if hou > 60:
                 hou = hou - 60
                 day +=1
-        sec = si - 60
+        sec = si - (60 *(si // 60))
 
     return hou, minu, sec
 
